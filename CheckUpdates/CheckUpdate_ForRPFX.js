767473185eeb0da89d4e8479f60f37e1c7c25317
tapioka-jp <suetake0301@outlook.jp>
2026-09-10 18:05:46 +0900

    Create CheckUpdate_ForRPFX.js

---
+++ CheckUpdates/CheckUpdate_ForRPFX.js
@@ -0,0 +1,9 @@
+//Created by : TP
+//For RPFX
+document.addEventListener("DOMContentLoaded", ()=> {
+    if(document.body.title === "1.1") {
+        console.log("log : accept");
+    }else{
+        alert(`Check Updata For RPFX :\nYour Version : Ver ${document.body.title}\nLatest Version : Ver 1.1\nDownload New In : https://github.com/TAPIOKA-X/TP_Tools`);
+    }
+});
